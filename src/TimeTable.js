d5bc9a83ce4f20b9782986d26ce3b647d62504c0
Edwin Roosevelt <bedwinroosevelt@gmail.com>
2022-03-30 01:50:43 +0530

    added type objects

--- src/TimeTable.js
+++ src/TimeTable.js
@@ -1,45 +1,57 @@
 class TimeTable {
-    daysPerWeek
-    periodsPerDay
-    slots = {}
-    subjectAndConstrain = []
+  daysPerWeek;
+  periodsPerDay;
+  slots = {};
+  subjectAndConstrains;
 
-    // Setting the initial variables and creating the empty slots
-    constructor(_daysPerWeek, _periodsPerDay, _subjectAndConstrain) {
-        this.daysPerWeek = _daysPerWeek
-        this.periodsPerDay = _periodsPerDay
-        this.intializeEmptySlots();
-        this.setSubjectAndConstrain(_subjectAndConstrain);
+  // Setting the initial variables and creating the empty slots
+  constructor(daysPerWeek, periodsPerDay, subjectAndConstrains) {
+    this.daysPerWeek = daysPerWeek;
+    this.periodsPerDay = periodsPerDay;
+    this.intializeEmptySlots();
+    this.subjectAndConstrains = subjectAndConstrains;
+  }
+
+  // creating the emply slots
+  intializeEmptySlots() {
+    for (var i = 1; i <= this.daysPerWeek; i++) {
+      for (var j = 1; j <= this.periodsPerDay; j++) {
+        this.slots[`${i}${j}`] = undefined;
+      }
     }
+  }
 
-    // creating the emply slots
-    intializeEmptySlots() {
-        for (var i = 1; i<=this.daysPerWeek; i++) {
-            for (var j=1; j<=this.periodsPerDay; j++) {
-                this.slots[`${i}${j}`] = undefined
-            }
-        }
+  isSlotsFilled() {
+    for (var i = 1; i <= this.daysPerWeek; i++) {
+      for (var j = 1; j <= this.periodsPerDay; j++) {
+        if (this.slots[`${i}${j}`] === undefined) return false;
+      }
     }
+    return true;
+  }
 
-    //initializing subjects with corresponding constrain
-    setSubjectAndConstrain() {
-        
+  allocate(subjectAndConstrains) {
+      const {subName, constrains} = subjectAndConstrains
+      var i = 1;
+      while(i <= constrains.type1) {
+          console.log("hi")
+          i++
+      }
+  }
+
+  type2Check(type, value, currentSlot) {
+    if (type === 2) {
+      var res = 0;
+      for (var i = 1; i <= this.daysPerWeek; i++) {
+        if (this.slots[`${i}${j}`] === "English") res += 1;
+      }
+      return true;
     }
+  }
 
-    isSlotsFilled() {
-        for (var i = 1; i <= this.daysPerWeek; i++) {
-          for (var j = 1; j <= this.periodsPerDay; j++) {
-              if (this.slots[`${i}${j}`] === undefined) return false
-          }
-        }
-        return true
-    }
-
-
-    allocate(subject, ) {
-
-    }
-
+  randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
 
 module.exports = TimeTable;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 const TimeTable = require("./TimeTable");
 
-var a = new TimeTable(5, 8)
+const daysPerWeek = 5
+const periodsPerDay = 8
+const subjectAndConstrains = [
+  {
+    subName: "English",
+    constrains: {
+      type1: 7,
+      type2: [1, 2],
+      type3: [1, 4, 1, 3],
+      type4: true,
+    },
+  },
+  {
+    subName: "Maths",
+    constrains: {
+      type1: 3,
+      type2: [1, 2],
+      type3: [1, 4],
+      type4: true,
+    },
+  },
+];
+    
 
-console.log(a.isSlotsFilled());
+var a = new TimeTable(daysPerWeek, periodsPerDay, subjectAndConstrains);
+
+// console.log(a.randomNumber(1, 4));
+a.allocate(subjectAndConstrains[1]);
 
